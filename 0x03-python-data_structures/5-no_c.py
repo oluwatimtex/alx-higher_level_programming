bb3b35635e2499d0674cd2a7f20918c93be51368
oluwatimtex <oluwatimtex@outlook.com>
2022-12-06 02:18:58 +0100

    remove all characters of c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for letter in range(len(my_string)):
-        if letter == c and letter == C:
+        if letter == "c" and letter == "C":
             return ""
         else:
             return letter
