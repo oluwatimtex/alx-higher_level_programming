3d329b10622fc36281591f80763f8be592013431
oluwatimtex <oluwatimtex@outlook.com>
2022-12-08 03:03:03 +0100

    adding huge numbers

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     total = 0
     for num in sys.argv:
-        if num != sys.argv[0:
+        if num != sys.argv[0]:
                 total += int(num)
     print(total)
